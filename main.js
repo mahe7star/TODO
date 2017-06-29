deb4bb7c26e1b511e881e6bc2657e6f39fabfeb0
mahe7star <mp1995singh@gmail.com>
2017-06-29 20:19:14 +0530

    ToDo list
    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Create a "close" button and append it to each list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
